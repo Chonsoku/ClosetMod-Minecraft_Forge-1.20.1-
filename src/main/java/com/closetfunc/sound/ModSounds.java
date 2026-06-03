5fe31d45ab029dcae234aa58f28722c34b78a5ca
Chesnok <aulmaxxx@gmail.com>
2026-06-03 20:14:19 +0300

    Update ModSounds.java

--- src/main/java/com/closetfunc/sound/ModSounds.java
+++ src/main/java/com/closetfunc/sound/ModSounds.java
@@ -7,7 +7,6 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
-@SuppressWarnings("removal")
 public class ModSounds {
     public static final DeferredRegister<SoundEvent> SOUNDS =
             DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, MainCloset.MOD_ID);
